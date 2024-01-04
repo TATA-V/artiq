01516a0fe16494093fcb3bea1379dab3c59e5a8e
Choi-hyeona <xhrl523@naver.com>
2024-01-04 09:22:06 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
-import { SessionProvider } from "next-auth/react"
-import { ComponentType } from 'react'
+// import { SessionProvider } from "next-auth/react"
+// import { ComponentType } from 'react'
 
-interface Props {
-  Component: ComponentType<any>;
-  pageProps: {
-    session: any;
-    [key: string]: any;
-  };
-}
+// interface Props {
+//   Component: ComponentType<any>;
+//   pageProps: {
+//     session: any;
+//     [key: string]: any;
+//   };
+// }
 
-export default function Home({ Component, pageProps: { session, ...pageProps } } : Props) {
+// export default function Home({ Component, pageProps: { session, ...pageProps } } : Props) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   )
+// }
+
+export default function Home() {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div>
+      main page
+    </div>
   )
 }
