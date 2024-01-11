2d066acfb9b5b79a8ae984dba280883d99f7db85
Choi-hyeona <xhrl523@naver.com>
2024-01-11 14:44:48 +0900

    feat: 소셜

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import { getProviders, signIn } from 'next-auth/react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 function SignInPage() {
   const [providers, setProviders] = useState(null)
@@ -16,26 +17,46 @@ function SignInPage() {
 
   return (
     <div className="p-20 flex flex-col items-center">
-      <button
-        className="w-[350px] h-[46px] font-medium transform rounded-md bg-[#03C95B] tracking-wide text-white transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
-        onClick={() => signIn('naver', { redirect: true, callbackUrl: '/' })}
-      >
-        naver login
-      </button>
+      <Link href="http://localhost:3001/naver">
+        <button className="w-[350px] h-[46px] font-medium transform rounded-md bg-[#03C95B] tracking-wide text-white transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none">
+          naver login
+        </button>
+      </Link>
 
-      {/* <button
-        className="w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-[#FBE300] tracking-wide text-[#3B1E1E] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
-        onClick={() => signIn("kakao", { redirect: true, callbackUrl: "/" })}
-      >
-        kakao login
-      </button> */}
+      <Link href="http://localhost:3001/kakao">
+        <button className="w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-[#FBE300] tracking-wide text-[#3B1E1E] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none">
+          kakao login
+        </button>
+      </Link>
 
-      <button
-        className="border-[1px] border-[#e5e5e5] w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-white tracking-wide text-[#383838] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
-        onClick={() => signIn('google', { redirect: true, callbackUrl: '/' })}
-      >
-        google login
-      </button>
+      <Link href="http://localhost:3001/google">
+        <button className="border-[1px] border-[#e5e5e5] w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-white tracking-wide text-[#383838] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none">
+          google login
+        </button>
+      </Link>
+
+      {/* test */}
+      <div className="flex flex-col mt-20">
+        <button
+          className="w-[350px] h-[46px] font-medium transform rounded-md bg-[#03C95B] tracking-wide text-white transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
+        >
+          naver login
+        </button>
+
+        <button
+          className="w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-[#FBE300] tracking-wide text-[#3B1E1E] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
+          onClick={() => signIn('kakao', { redirect: true, callbackUrl: '/' })}
+        >
+          kakao login
+        </button>
+
+        <button
+          className="border-[1px] border-[#e5e5e5] w-[350px] h-[46px] mt-3 font-medium transform rounded-md bg-white tracking-wide text-[#383838] transition-colors duration-200 hover:opacity-80 focus:opacity-85 focus:outline-none"
+          onClick={() => signIn('google', { redirect: true, callbackUrl: '/' })}
+        >
+          google login
+        </button>
+      </div>
 
       <Link href="/">
         <div className="w-[350px] first-line:text-[14px] pt-[20px]">뒤로가기</div>
