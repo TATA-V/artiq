667faaf83b4fe07b39c9044c7466ffeff46d0cba
Choi-hyeona <xhrl523@naver.com>
2024-01-15 14:33:03 +0900

    chore: next-config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${NEXT_PUBLIC_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
       },
     ];
   },
